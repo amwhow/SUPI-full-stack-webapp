c9cc07a626b24b3d948400475ff55ea635333672
Mark <amwhow@gmail.com>
2021-02-03 21:41:58 +1100

    refactored the routes for user auth with style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react'
-import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom"
-import Dashboard from './components/dashboard_folder/Dashboard'
-import NewUser from './components/user/NewUser'
+import { React, useState, useEffect, useContext } from 'react'
+import { Route, Switch, Link, useHistory } from "react-router-dom";
 import Login from './components/user/Login'
-import ProtectedRoute from './components/ProtectedRoute'
+import LoggedOutNav from './components/user/LoggedOutNav'
+import Dashboard from "./components/dashboard_folder/Dashboard";
+import NewUser from "./components/user/NewUser";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Home from "./components/Home"
 
 function App() {
-  return (
-    // if not logged in , show log in page, else show dashboard page
-    <Router >
-      <ul>
-        <li><Link to="/dashboard">Home</Link></li>
-        <li><Link to="/signup">Sign Up</Link></li>
-        <li><Link to="/login">Login</Link></li>
-      </ul>
 
-      <Switch>  
+  return (
+    <div>
+      <Switch>
         <ProtectedRoute exact path="/dashboard" component={Dashboard} />
         <Route exact path="/signup" component={NewUser} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/" component={Home} />
       </Switch>
-    </Router >
+    </div>
   );
 }
 
---
+++ src/components/styles/Form.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  .form-content {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    label {
+      font-size: 1.5rem;
+      margin-bottom: 5px;
+    }
+    input {
+      height: 25px;
+      font-size: 20px;
+    }
+    #submit {
+      width: 100px;
+      margin-top: 20px;
+    }
+  }
+`
+
+export const Container = styled.div`
+  width: 80%;
+  margin: 0 auto;
+`
