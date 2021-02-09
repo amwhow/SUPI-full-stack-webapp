e921f2ef3bb9b236e3bdbe435d378e9b68416e5b
Mark <amwhow@gmail.com>
2021-02-09 20:48:36 +1100

    Revert "fixed about page and listitem redirecting issue"
    This reverts commit 065001d6e294807bd5488b194562dbb5611d4da6.

--- src/components/dashboard_folder/listItems.js
+++ src/components/dashboard_folder/listItems.js
@@ -70,7 +70,7 @@ export function MainListItems({history, suppliers}) {
                     button
                     className={classes.nested}
                     onClick={() => {
-                      history.push(`/dashboard/supplier/${element.id}`)
+                      history.push(`/dashboard/supplier/${id}`)
                       // getSupplierShow(history, element.id, element)
                     }}
                   >
