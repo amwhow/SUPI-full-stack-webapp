2408096fbd41fbc2adf155a05698fd9e261bcfb9
Mark <amwhow@gmail.com>
2021-03-22 17:27:18 +1100

    added supplierId for rendering condition

--- src/components/dashboard_folder/supplier_info/PurchaseOrders.js
+++ src/components/dashboard_folder/supplier_info/PurchaseOrders.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import POTable from "../../table/POTable";
+import { useParams } from "react-router-dom";
 
 export default function PurchaseOrders() {
+  const { id } = useParams();
   return (
     <Grid container spacing={3} direction="row" justify="flex-start">
       <Grid item xs={12} md={6} lg={6}>
-        <POTable />
+        <POTable supplierId={id}/>
       </Grid>
     </Grid>
   );
